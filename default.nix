15767f6e37a2cca53b6bf9c215f90b06b30b677b
Sridhar Ratnakumar <srid@srid.ca>
2020-07-07 20:10:55 -0400

    Enable hoogle

--- default.nix
+++ default.nix
@@ -2,11 +2,15 @@ let
   nixpkgsRev = "4855aa62fa13";
   pkgs = import (builtins.fetchTarball {
       url = "https://github.com/nixos/nixpkgs/archive/${nixpkgsRev}.tar.gz";
-      sha256 = "sha256:0ncsh5rkjbcdaksngmn7apiq1qhm5z1z6xa1x70svxq91znibp4f";
+      sha256 = "sha256:17sk264qw397zzw12x11ry5vj9qidgbmjsyj544ysg857b4qq9sj";
     }) { };
 in 
   pkgs.haskellPackages.developPackage {
     root = ./.;
+    overrides = self: super: with pkgs.haskell.lib; {
+      ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
+      ghcWithPackages = self.ghc.withPackages;
+    };
     modifier = drv:
       pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
         [ cabal-install
