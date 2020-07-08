86617ef2e732a10160aa364be5fd475e2f228a37
Sridhar Ratnakumar <srid@srid.ca>
2020-07-07 20:05:26 -0400

    New brand

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let 
-  nixpkgsRev = "571212eb839d";
+  nixpkgsRev = "4855aa62fa13";
   pkgs = import (builtins.fetchTarball {
       url = "https://github.com/nixos/nixpkgs/archive/${nixpkgsRev}.tar.gz";
       sha256 = "sha256:0ncsh5rkjbcdaksngmn7apiq1qhm5z1z6xa1x70svxq91znibp4f";
